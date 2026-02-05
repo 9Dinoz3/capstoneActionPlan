79a425e6870bcc5684902de5f4733b7ea90ed1a7
9Dinoz3 <lucas.lovett@rutlandrobotics.org>
2026-02-05 10:57:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+  const sections = document.getElementsByClassName('section');
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.display = 'none';
+  }
+  
+  document.getElementById(sectionId).style.display = 'block';
+}
+
+showSection('home');

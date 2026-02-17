bb61680d4bf25e0dfc98b65117f239c3d148a67b
9Dinoz3 <lucas.lovett@rutlandrobotics.org>
2026-02-17 09:23:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function showSection(sectionId) {
-  const sections = document.getElementsByClassName('section');
+  const sections = document.getElementsByClassName("section");
   for (let i = 0; i < sections.length; i++) {
-    sections[i].style.display = 'none';
+    sections[i].style.display = "none";
   }
-  
-  document.getElementById(sectionId).style.display = 'block';
+
+  document.getElementById(sectionId).style.display = "block";
   window.scrollTo(0, 0);
 }
-showSection('home');
+showSection("home");
